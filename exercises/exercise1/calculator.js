0850a0b0786b8853e2823d49d0b60b6d5a4ebca1
Christopher Hasbani <chasbani@umd.edu>
2022-01-08 22:31:06 -0500

    exercise 1

--- exercises/exercise1/calculator.js
+++ exercises/exercise1/calculator.js
@@ -1,9 +1,13 @@
+const modules = require("./functions.js");
 
 function main() {
-    console.log('runing script......')
+    console.log('running script')
     // call your functions here
-
-    console.log('ending script......')
+    console.log(modules.add(12,3));
+    console.log(modules.subtract(12,3));
+    console.log(modules.multiply(12,3));
+    console.log(modules.divide(12,3));
+    console.log('done running script')
 }
 
 main()
